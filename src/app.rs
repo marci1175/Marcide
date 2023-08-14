0a0d586bcd2d850ba0ccf7d7ab793d8d6a21719f
Varga Marcell <v.marcell4000@gmail.com>
2023-08-14 19:02:02 +0200

    Adds to PATH sys var

--- src/app.rs
+++ src/app.rs
@@ -5,6 +5,8 @@ use egui::{Color32, RichText, TextBuffer, Vec2};
 use rfd::FileDialog;
 use std::io;
 use std::env;
+use winreg::enums::*;
+use winreg::RegKey;
 use std::path::PathBuf;
 use std::sync::mpsc;
 use windows_sys::w;
@@ -498,6 +500,34 @@ impl eframe::App for TemplateApp {
                 .show(ctx, |ui| {
                     ui.label(RichText::from("Application").size(20.0));
                     if ui.button("Add to windows context menu").clicked() {
+                        let path = format!("{}", env::current_dir().unwrap().display());
+                        let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
+                        let environment_key = hklm.open_subkey_with_flags(
+                            r"SYSTEM\CurrentControlSet\Control\Session Manager\Environment",
+                            KEY_READ | KEY_WRITE,
+                        );
+
+                        match environment_key {
+                            Ok(key) => {
+                                // Read the existing PATH value
+                                let mut path_value: String = key.get_value("PATH").unwrap_or_default();
+
+                                // Append the new path to the existing value
+                                if !path_value.is_empty() {
+                                    path_value.push_str(";");
+                                }
+                                path_value.push_str(&path);
+
+                                // Set the modified PATH value
+                                key.set_value("PATH", &path_value).expect("Failed to set value");
+
+                                // Notify the user about the updated PATH
+                            }
+                            Err(err) => {
+                                eprintln!("Error: {}", err);
+                            }
+                        }
+                        
                         if let Ok(exe_path) = env::current_exe() {
                             let app_path = exe_path;
                             let _ = std::process::Command::new("reg")
@@ -530,6 +560,35 @@ impl eframe::App for TemplateApp {
                         }
                     };
                     if ui.button("Remove from windows context menu").clicked() {
+                        let path = format!("{}", env::current_dir().unwrap().display());
+                        let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
+                        let environment_key = hklm.open_subkey_with_flags(
+                            r"SYSTEM\CurrentControlSet\Control\Session Manager\Environment",
+                            KEY_READ | KEY_WRITE,
+                        );
+
+                        match environment_key {
+                            Ok(key) => {
+                                // Read the existing PATH value
+                                let path_value: String = key.get_value("Path").unwrap_or_default();
+
+                                // Split the existing paths by semicolon
+                                let paths: Vec<_> = path_value.split(';').collect();
+
+                                // Create a new PATH value without the path to remove
+                                let new_paths: Vec<_> = paths.into_iter().filter(|&p| p != path).collect();
+                                let new_path_value = new_paths.join(";");
+
+                                // Set the modified PATH value
+                                key.set_value("Path", &new_path_value).expect("Failed to set value");
+
+                                // Notify the user about the updated PATH
+                                println!("Updated PATH: {}", new_path_value);
+                            }
+                            Err(err) => {
+                                eprintln!("Error: {}", err);
+                            }
+                        }
                         let _ = std::process::Command::new("reg")
                             .args(&[
                                 "delete",
